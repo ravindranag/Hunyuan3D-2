d92bb56789d33d60d707c8d9d2f19c7fa34f5d04
Ravindra Nag <ravindranag52@gmail.com>
2025-08-11 00:54:52 +0530

    chore: public ip env

--- gradio_app_env.sh
+++ gradio_app_env.sh
@@ -2,11 +2,11 @@
 
 # Retrieve the public IP and token
 TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-PUBLIC_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
+PUBLIC_IPV4=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/public-ipv4)
 
 # Write the environment variables to a file that will be sourced for new shells
-echo "export PUBLIC_IP=$PUBLIC_IP" > /etc/profile.d/gradio_app_env.sh
-echo "export TOKEN=$TOKEN" >> /etc/profile.d/gradio_app_env.sh
+sudo echo "export PUBLIC_IPV4=$PUBLIC_IPV4" > /etc/profile.d/gradio_app_env.sh
+sudo echo "export TOKEN=$TOKEN" >> /etc/profile.d/gradio_app_env.sh
 
 # Make the new script executable
 chmod +x /etc/profile.d/gradio_app_env.sh
